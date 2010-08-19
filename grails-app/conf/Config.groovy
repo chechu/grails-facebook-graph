90fc6adb0602a870497284daa159fd324cfa38f6
Jesus Lanchas <jesus.lanchas@daureos.com>
2010-08-19 15:40:04 +0000

    Upgraded to grails 1.3.4

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -22,3 +22,7 @@ log4j = {
 
     warn   'org.mortbay.log'
 }
+
+// The following properties have been added by the Upgrade process...
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
