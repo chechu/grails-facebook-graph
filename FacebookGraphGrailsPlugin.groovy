72725e8807d6a22cdc1c5d830f8cce6403c49172
Jesus Lanchas <jlanchas@acceso.com>
2012-01-01 13:30:47 +0100

    New plugin version

--- FacebookGraphGrailsPlugin.groovy
+++ FacebookGraphGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class FacebookGraphGrailsPlugin {
     // the plugin version
-    def version = "0.12"
+    def version = "0.13"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.3 > *"
     // the other plugins this plugin depends on
